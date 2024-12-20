e3399a8e8b30b726eba8fa9c18afc9c6d120eca7
Rahul Sutradhar <rahulsd380@gmail.com>
2024-12-20 12:40:32 +0600

    Added formatDate function

--- src/components/SubscriptionsPageComponents/SubscriptionsCard/SubscriptionsCard.tsx
+++ src/components/SubscriptionsPageComponents/SubscriptionsCard/SubscriptionsCard.tsx
@@ -1,28 +1,44 @@
 import { Link } from "react-router-dom";
+import { formatDate } from "../../../utils/formatDate";
 
-const SubscriptionsCard = ({ variant, name, duration, endDate }: { variant: string }) => {
+type TSubscriptionCard = {
+  variant: string;
+  name: string;
+  duration: string;
+  endDate: string;
+};
+const SubscriptionsCard: React.FC<TSubscriptionCard> = ({
+  variant,
+  name,
+  duration,
+  endDate,
+}) => {
   return (
     <div className="font-Poppins flex flex-col md:flex-row gap-4 md:gap-0 items-start md:items-center justify-between bg-white px-6 py-5 border border-[#D4D7DB] rounded-xl">
       <div>
         <h1 className="text-[#424B54] text-lg font-medium leading-6">
           {name} ({duration})
         </h1>
-        <p className="text-[#6E7883] leading-5">Expires on {endDate}</p>
+        <p className="text-[#6E7883] leading-5">
+          Expires on {formatDate(endDate)}
+        </p>
       </div>
 
       <div className="flex items-center gap-4">
         <div
           className={`${
-            variant === "Active" ? "bg-[#DCFED6]" : variant === "Pending" ? "bg-yellow-400/50" : "bg-[#f5ced1]"
+            variant === "Active"
+              ? "bg-[#DCFED6]"
+              : variant === "Pending"
+              ? "bg-yellow-400/40"
+              : "bg-[#f5ced1]"
           }  rounded-lg px-3 py-2 text-[#24461F] font-medium leading-5 flex items-center justify-center`}
         >
-          {
-            variant === "Active"
-             ? "Active"
-              : variant === "Pending"
-             ? "Pending"
-              : "Expired"
-          }
+          {variant === "Active"
+            ? "Active"
+            : variant === "Pending"
+            ? "Pending"
+            : "Expired"}
         </div>
         {variant === "Expired" && (
           <Link
